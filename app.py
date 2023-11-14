7d49e00db36772c878e3fdf3e8c70867b6889bc3
Uchechukwu Emmanuel <daleentontech@gmail.com>
2023-11-14 11:59:39 +0100

    add prediction pipeline

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
 import numpy as np
 import pandas as pd
-
+from flask import Flask, flash, redirect, render_template, request, url_for
 from sklearn.preprocessing import StandardScaler
+
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
 
@@ -12,10 +13,26 @@ def index():
     return render_template('index.html')
 
 @app.route('/predict', methods=['GET', 'POST'])
-# def predict():
-#     if request.method == 'POST':
-#         age = request
-#         age = int(age)
-#         return render_template('predict.html', age=age)
-#
-#     return render_template('predict.html')
+def predict():
+    if request.method == 'GET':
+        return render_template('home.html')
+    elif request.method == 'POST':
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=request.form.get('reading_score'),
+            writing_score=request.form.get('writing_score')
+        )
+        df = data.get_data_as_df()
+        print("df: ", df)
+        predict_pipeline = PredictPipeline()
+        result = predict_pipeline.predict(df)
+        print("result: ", result)
+        return render_template('home.html', result=result[0])
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=True)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,7 @@
 import sys
+
 import pandas as pd
+
 from src.exception import CustomException
 from src.utils import load_obj
 
