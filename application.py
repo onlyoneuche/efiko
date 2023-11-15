a5e1c2c513e2793c6c9a83a9c3cf0fa10af02202
Uchechukwu Emmanuel <daleentontech@gmail.com>
2023-11-15 12:28:24 +0100

    rename module

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
-
+application= Flask(__name__)
+app = application
 
 @app.route('/')
 def index():
